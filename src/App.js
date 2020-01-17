e48bdd0c5209735b4ddec3e06184ce750bccacd7
田中満久 <mitsuhisatanaka@gmail.com>
2020-01-17 22:54:11 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
+// クラスComponent
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     )
+//     // const greeting = "Hi, Tom!";
+//     // const dom = <h1>{greeting}</h1>;
+//     // return dom;
+//   }
+// }
+
+// ファンクショナルComponent
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
