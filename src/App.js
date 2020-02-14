def735ab48a756649086a90c926b13531cdcbe34
田中満久 <mitsuhisatanaka@gmail.com>
2020-02-14 15:35:01 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// クラスComponent
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </React.Fragment>
-//     )
-//     // const greeting = "Hi, Tom!";
-//     // const dom = <h1>{greeting}</h1>;
-//     // return dom;
-//   }
-// }
-
-// ファンクショナルComponent
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 20 },
+    { name: "hanako", age: 30 },
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number
+}
+
 
 export default App;
